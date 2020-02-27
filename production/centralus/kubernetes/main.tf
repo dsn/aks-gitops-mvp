ed61c98081b657f7de6a337960efc138866ab2d3
haisumb <haisum@boxboat.com>
2020-02-27 11:14:31 -0500

    Fixed provider config

--- production/centralus/kubernetes/main.tf
+++ production/centralus/kubernetes/main.tf
@@ -13,13 +13,23 @@ data "azurerm_kubernetes_cluster" "main" {
   name                = var.cluster_name
 }
 
+# When using without Active Directory and RBAC, uncomment following
+# provider "kubernetes" {
+#   version                = "=1.10"
+#   host                   = "${data.azurerm_kubernetes_cluster.main.kube_config.0.host}"
+#   username               = "${data.azurerm_kubernetes_cluster.main.kube_config.0.username}"
+#   password               = "${data.azurerm_kubernetes_cluster.main.kube_config.0.password}"
+#   client_certificate     = "${base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)}"
+#   client_key             = "${base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_key)}"
+#   cluster_ca_certificate = "${base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)}"
+# }
+
 
 provider "kubernetes" {
   version                = "=1.10"
   host                   = data.azurerm_kubernetes_cluster.main.kube_config.0.host
-  username               = data.azurerm_kubernetes_cluster.main.kube_config.0.username
-  password               = data.azurerm_kubernetes_cluster.main.kube_config.0.password
-  client_certificate     = data.azurerm_kubernetes_cluster.main.kube_config.0.client_certificate
-  client_key             = data.azurerm_kubernetes_cluster.main.kube_config.0.client_key
-  cluster_ca_certificate = data.azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.main.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.main.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.main.kube_admin_config.0.cluster_ca_certificate)
+  load_config_file       = "false"
 }

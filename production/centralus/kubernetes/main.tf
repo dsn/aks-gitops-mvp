d9df5c69e4522cd78275a01ea1a269bc57f06aba
haisumb <haisum@boxboat.com>
2020-03-05 21:20:04 -0500

    disabled loading config file

--- production/centralus/kubernetes/main.tf
+++ production/centralus/kubernetes/main.tf
@@ -41,5 +41,6 @@ provider "helm" {
     client_certificate     = base64decode(data.azurerm_kubernetes_cluster.main.kube_admin_config.0.client_certificate)
     client_key             = base64decode(data.azurerm_kubernetes_cluster.main.kube_admin_config.0.client_key)
     cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.main.kube_admin_config.0.cluster_ca_certificate)
+    load_config_file       = "false"
   }
 }
